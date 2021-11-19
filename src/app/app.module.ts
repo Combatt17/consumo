967a6c16a505cfba19faf20bbb707cb11a0f7ceb
Combatt17 <carloscombatt17@gmail.com>
2021-11-19 06:29:07 -0500

    Proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EquipoComponent } from './ejemplo/equipo/equipo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EquipoComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })
